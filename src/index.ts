a03f1b04911bfa00c76710e8bd35b772d3e78e7a
Solant <runner62v6@gmail.com>
2021-10-22 14:20:55 +0300

    feat: add auto updater function

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@
 
 import { program } from 'commander';
 import chalk from 'chalk';
+import updateNotifier from 'update-notifier';
 
 import generate from './commands/generate';
 
-program.version(`${chalk.yellow('Flatlogic Apps CLI')} ${require('../package.json').version}`);
+const pkg = require('../package.json');
+
+program.version(`${chalk.yellow('Flatlogic Apps CLI')} ${pkg.version}`);
 
 program.addCommand(generate);
+
+updateNotifier({
+    pkg,
+    updateCheckInterval: 1000 * 60 * 60 * 24, // 1 day
+}).notify();
 
 program.parse(process.argv);
